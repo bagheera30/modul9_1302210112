118546a55e42142d7a0849921ed2a2306d12118a
bagheera30 <rizkialfian30103@gmail.com>
2023-04-26 17:20:36 +0700

    menambahkan api modvie

---
+++ modul9_1302210112/Controllers/moviesController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace modul9_1302210112.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class moviesController : ControllerBase
+    {
+          
+        // GET: api/<movie>
+        [HttpGet]
+        public IEnumerable<movie> Get()
+        {
+            return movie.Movies;
+        }
+
+        // GET api/<movie>/5
+        [HttpGet("{id}")]
+        public ActionResult<movie> Get(int id)
+        {
+            try
+            {
+                if (movie.Movies == null || id < 0 || id >= movie.Movies.Count)
+                {
+                    return NotFound("movie.Movies mahasiswa tidak ditemukan");
+                }
+                return movie.Movies[id];
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<movie>
+        [HttpPost]
+        public void Post([FromBody] movie value)
+        {
+            if (value == null)
+            {
+                BadRequest("Data mahasiswa tidak valid");
+                return;
+            }
+            movie.Movies.Add(value);
+            CreatedAtAction(nameof(Get), new { id = movie.Movies.IndexOf(value) }, value);
+        }
+
+     
+
+        // DELETE api/<movie>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            try
+            {
+                if (movie.Movies == null || id < 0 || id >= movie.Movies.Count)
+                {
+                    NotFound("Data mahasiswa tidak ditemukan"); // Return a NotFound response if movie.Movies is null, or id is out of range
+                    return;
+                }
+
+                movie.Movies.RemoveAt(id);
+                NoContent();
+            }
+            catch (Exception ex)
+            {
+                BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ modul9_1302210112/movie.cs
@@ -0,0 +1,20 @@
+﻿using modul9_1302210112.Controllers;
+
+namespace modul9_1302210112
+{
+    public class movie
+    {
+        public static List<movie> Movies = new List<movie>()
+        {
+            new movie() { title = "The Shawshank Redemption", director = "Frank Darabont", description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", star = new List<string>(){  "Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler" } },
+            new movie() { title = "The Godfather", director = "Francis Ford Coppola", description = "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.", star = new List<string>(){ "Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton" } },
+            new movie() { title = "The Dark Knight", director = "Christopher Nolan", description = "TWhen the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", star = new List<string>(){ "Christian Bale", "Heath Ledger", "Aaron Eckhart", "Michael Caine" } },
+            new movie() { title = "The Lord of the Rings: The Return of the King", director = "Peter Jackson", description = "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.", star = new List<string>(){ "Elijah Wood", "Viggo Mortensen", "Ian McKellen", "Orlando Bloom" } }
+        };
+        public string title { get; set; }
+        public string director { get; set; }
+        public List<string> star{ get; set;}
+        public string description { get; set; }
+        public movie() { }
+    }
+}
